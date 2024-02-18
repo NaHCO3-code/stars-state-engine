c5c827dba290a652acda09cf4e553dc791ae4068
Hulue <80567300+NaHCO3-code@users.noreply.github.com>
2024-02-18 22:31:32 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "/node-magician",
+  base: "/muti-body",
   build:{
     outDir: "docs",
   },
